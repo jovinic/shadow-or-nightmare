c4b7cda237fc1288274740c78086da54575c51c1
zalmjoa <jvalmeida@fitec.org.br>
2019-04-10 10:08:58 -0300

    Initial commit after cleanup

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private Rigidbody2D myBody;
+    private Animator animator;
+    public float speed;
+    private Vector3 originalScale;
+    public GameObject tBear;
+    public float throwPower;
+    public bool canThrow;
+
+    void Start()
+    {
+        myBody = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        originalScale = transform.localScale;
+        canThrow = true;
+    }
+
+    void FixedUpdate()
+    {
+        Movement();
+    }
+
+    void Update()
+    {
+        Throw();
+    }
+
+    void Movement()
+    {
+        float h = Input.GetAxisRaw("Horizontal");
+        if (h != 0)
+        {
+            myBody.velocity = new Vector2(speed * h, myBody.velocity.y);
+
+            Vector3 scale = transform.localScale;
+            scale.x = h > 0 ? originalScale.x : -originalScale.x;
+            transform.localScale = scale;
+
+            animator.SetBool("Run", true);
+        }
+        else
+        {
+            animator.SetBool("Run", false);
+        }        
+    }
+
+    void Throw()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            if (!canThrow)
+                return;                
+
+            canThrow = false;
+            animator.SetTrigger("Throw");
+
+            GameObject newTBear = Instantiate(tBear,
+                                              transform.position,
+                                              tBear.transform.rotation);
+
+            // throw towards mouse position
+            Vector3 sp = Camera.main.WorldToScreenPoint(transform.position);
+            Vector3 dir = (Input.mousePosition - sp).normalized;
+            newTBear.GetComponent<Rigidbody2D>().AddForce(dir * throwPower);
+            
+            // set scale for object position (todo)
+            
+        }
+    }
+}
---
+++ Assets/Scripts/TBear.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TBear : MonoBehaviour
+{
+    public float timer;
+    public float colDelay;
+
+    void Start()
+    {        
+    }
+
+    void Update()
+    {
+        timer -= Time.deltaTime;
+        
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>().position = gameObject.GetComponent<Transform>().position;
+            
+            destroyTBear();
+            return;
+        }
+
+        if(timer <= 0)
+        {
+            destroyTBear();
+        }
+    }
+
+    void destroyTBear()
+    {
+        GameObject.FindGameObjectWithTag("Player").GetComponent<Player>().canThrow = true;
+        Destroy(gameObject);
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        if(col.gameObject.tag != "Player")
+        {
+            //Physics.IgnoreCollision(GameObject.FindGameObjectWithTag("Player").GetComponent<Collider>(), gameObject.GetComponent<Collider>());            
+            timer = 0;
+        }
+    }
+}
