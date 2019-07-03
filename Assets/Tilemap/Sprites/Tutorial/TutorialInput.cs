26b956e490352a42a653f76ca4b3fa4e1b5db884
Joao Vinicius Almeida <joaom_almeida@live.com>
2019-07-02 21:57:34 -0300

    added floating hint

---
+++ Assets/Tilemap/Sprites/Tutorial/TutorialInput.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialInput : MonoBehaviour
+{
+    void Start()
+    {
+        StartCoroutine(WaitForWalkInput());        
+    }
+
+    IEnumerator WaitForWalkInput()
+    {    
+        float axis = 0;
+        while(axis == 0)
+        {
+            axis = Input.GetAxis ("Horizontal");
+            yield return null;
+        }            
+
+        Destroy(this.gameObject);
+    }
+}
