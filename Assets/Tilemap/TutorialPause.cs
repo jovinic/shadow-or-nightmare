a5bcce0a8c4d7eab4f0ddd6e894dd54a9dee38b3
Joao Vinicius Almeida <joaom_almeida@live.com>
2019-07-11 22:05:20 -0300

    Added player freeze for tutorial

---
+++ Assets/Tilemap/TutorialPause.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialPause : MonoBehaviour
+{
+    private bool playerBlocked;
+    private RigidbodyConstraints2D playerConstraints;
+    
+    void Start()
+    {
+        playerBlocked = false;
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == "Player" && !playerBlocked)
+        {            
+            other.gameObject.GetComponent<Movement>().playerLevel = 1;
+            other.gameObject.GetComponent<Movement>().canMove = false;
+            playerConstraints = other.gameObject.GetComponent<Rigidbody2D>().constraints;
+            other.gameObject.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezeAll;
+
+            playerBlocked = true;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if(other.tag == "Player" && playerBlocked)
+        {            
+            other.gameObject.GetComponent<Movement>().canMove = true;
+            other.gameObject.GetComponent<Rigidbody2D>().constraints = playerConstraints;
+        }
+    }
+}
