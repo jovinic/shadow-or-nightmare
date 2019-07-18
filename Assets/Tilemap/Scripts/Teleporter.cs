2afebacd67e39528ce8b86a3058d9d74bba601a4
Joao Vinicius Almeida <joaom_almeida@live.com>
2019-07-17 22:00:30 -0300

    Added fade-in transition

---
+++ Assets/Tilemap/Scripts/Teleporter.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleporter : MonoBehaviour
+{
+    public Transform destination;
+    public Animator transitionAnim;
+
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == "Player")
+        {
+            StartCoroutine(TeleportPlayer(other.gameObject)); 
+        }
+    }
+
+    IEnumerator TeleportPlayer(GameObject player)
+    {
+        player.GetComponent<Movement>().FreezePlayer();
+
+        transitionAnim.SetTrigger("End");
+        yield return new WaitForSeconds(transitionAnim.GetCurrentAnimatorStateInfo(0).length);
+        
+        player.GetComponent<Movement>().TeleportPlayer(destination);
+        player.GetComponent<Movement>().UnfreezePlayer();
+    }
+}
