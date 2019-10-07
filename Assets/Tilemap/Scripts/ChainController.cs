c619f01f9812f330ea9f5494d02729c13fd2cff0
Joao Vinicius Almeida <joaom_almeida@live.com>
2019-10-07 15:58:50 -0300

    trying to add chain destruction

---
+++ Assets/Tilemap/Scripts/ChainController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChainController : MonoBehaviour
+{
+    public List<int> destroyedChain = new List<int>();
+    private bool isProcessingList = false;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        if(destroyedChain.Count == 0 || isProcessingList)
+        {
+            return;
+        }
+        else
+        {
+            isProcessingList = true;
+            foreach(int i in destroyedChain)
+            {
+                Transform chain = transform.Find("chain"+i);
+                chain.gameObject.GetComponent<ReleasableJoint>().canDestroy = true;
+
+                if ((i-1) > 0)
+                {
+                    destroyedChain.Add(i-1);
+                }
+
+                if ((i+1) <= destroyedChain.Count)
+                {
+                    destroyedChain.Add(i+1);
+                }
+
+                break;
+            }
+            isProcessingList = false;
+        }
+    }
+}
---
+++ Assets/Tilemap/Scripts/ReleasableJoint.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReleasableJoint : MonoBehaviour
+{
+    public int orderSeq;
+    public bool canDestroy = false;
+
+    void Update()
+    {
+        if (!canDestroy)
+        {
+            return;
+        }
+
+        Destroy(this.gameObject);
+    }
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == "TBear")
+        {
+            transform.parent.gameObject.GetComponent<ChainController>().destroyedChain.Add(orderSeq);
+        }
+    }
+}
