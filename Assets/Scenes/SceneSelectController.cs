2ad5ea0a124b586f3e774b5af199dc7f29a78c32
zalmjoa <jvalmeida@fitec.org.br>
2019-10-08 14:04:33 -0300

    added main menu

---
+++ Assets/Scenes/SceneSelectController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSelectController : MonoBehaviour
+{
+    public void selectScene()
+    {
+        switch(this.gameObject.name)
+        {
+            case "SceneButton001":
+                SceneManager.LoadScene("001A-House");
+                break;
+            case "SceneButton002":
+                SceneManager.LoadScene("002A-DreamWorld");
+                break;
+            case "SceneButton003":
+                SceneManager.LoadScene("003A-DreamWorld");
+                break;
+            case "SceneButton004":
+                SceneManager.LoadScene("004A-DreamWorld");
+                break;
+        }
+    }
+}
