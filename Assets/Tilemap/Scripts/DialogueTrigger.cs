456e24ea36c3e5c8b1af2035a94211bd97ecd87f
Joao Vinicius Almeida <joaom_almeida@live.com>
2019-10-23 22:11:45 -0300

    Finished dialogue

---
+++ Assets/Tilemap/Scripts/DialogueTrigger.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueTrigger : MonoBehaviour
+{
+    public Dialogue dialogue;
+    private bool dialogueStarted = false;
+
+    void Update()
+    {
+        if(dialogueStarted)
+        {
+            return;
+        }
+
+        dialogueStarted = true;
+        StartCoroutine(WaitDialogueDelay(1f));
+    }
+
+    IEnumerator WaitDialogueDelay(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+
+        TriggerDialogue();
+    }
+
+    public void TriggerDialogue()
+    {
+        FindObjectOfType<DialogueManager>().StartDialogue(dialogue);
+    }
+}
