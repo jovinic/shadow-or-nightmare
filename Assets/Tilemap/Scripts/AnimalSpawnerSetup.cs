3c39469b4e795860b160336758abeda69d5cd8ba
Joao Vinicius Almeida <joaom_almeida@live.com>
2019-10-28 21:14:45 -0300

    removed unnecessary code from animal spawner

--- Assets/Tilemap/Scripts/AnimalSpawnerSetup.cs
+++ Assets/Tilemap/Scripts/AnimalSpawnerSetup.cs
@@ -8,18 +8,23 @@ public class AnimalSpawnerSetup : MonoBehaviour
     public List<string> animals;
     public Transform spawnBegin;
     public Transform spawnEnd;
+    private int partitionCount;
 
     public void SpawnAnimals()
     {
+        float xPartition = (spawnEnd.position.x - spawnBegin.position.x) / animals.Count;
+        partitionCount = 0;
+
         foreach (string animal in animals)
         {
             Vector3 randomPosition =
-                        new Vector3(Random.Range(spawnBegin.position.x, spawnEnd.position.x),
-                        spawnEnd.position.y,
-                        0);
+                        new Vector3(spawnBegin.position.x + (xPartition * partitionCount),
+                                    spawnEnd.position.y,
+                                    0);
             GameObject newAnimal = Instantiate(releasedAnimalGenerator, randomPosition, transform.rotation);
-            newAnimal.GetComponent<ReleasedAnimalController>().maxHeight = spawnBegin.position.y;
             newAnimal.GetComponent<Animator>().SetTrigger(animal);
+
+            partitionCount++;
         }
     }
 }
--- Assets/Tilemap/Scripts/ReleasedAnimalController.cs
+++ Assets/Tilemap/Scripts/ReleasedAnimalController.cs
@@ -4,34 +4,10 @@ using UnityEngine;
 
 public class ReleasedAnimalController : MonoBehaviour
 {
-    public float minSpeed;
-    public float maxSpeed;
-    private float moveSpeed;
-
-    public float maxHeight;
-    private Vector3 nextPosition;
-    private Vector3 currentPosition;
     private SpriteRenderer spriteRenderer;
     void Start()
     {
         spriteRenderer = GetComponent<SpriteRenderer>();
         spriteRenderer.flipX = (Random.value > 0.5f);
-
-        currentPosition = transform.position;
-        nextPosition = new Vector3(transform.position.x, maxHeight, transform.position.z);
-        moveSpeed = Random.Range(minSpeed, maxSpeed);
-    }
-
-    void Update()
-    {
-        transform.position = Vector2.MoveTowards(transform.position,
-                                                 nextPosition,
-                                                 Time.deltaTime * moveSpeed);
-
-        if(transform.position == nextPosition)
-        {
-            nextPosition = currentPosition;
-            currentPosition = transform.position;
-        }
     }
 }
